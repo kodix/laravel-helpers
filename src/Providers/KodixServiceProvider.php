e9fa127e5907a797cbfe511690515d77b48425b6
malyusha <im@kodix.ru>
2018-08-16 14:30:19 +0300

    Added service provider

---
+++ src/Providers/KodixServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file is a part of Yamaha Web CIS project.
+ * Email:       support@kodix.ru
+ * Company:     Kodix LLC <https://kodix.com>
+ * Developer:   Igor Malyuk <https://github.com/malyusha>
+ */
+
+namespace Kodix\LaravelHelpers\Providers;
+
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+
+class KodixServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->registerBlueprintMacros();
+    }
+
+    protected function registerBlueprintMacros(): void
+    {
+        Blueprint::macro('weight', function ($defaultOrder = 10) {
+            /**@var Blueprint $this */
+            return $this->integer('weight')->nullable()->default($defaultOrder);
+        });
+
+        Blueprint::macro('meta', function () {
+            /**@var Blueprint $this */
+            return $this->jsonb('meta')->default('{}');
+        });
+
+        Blueprint::macro('uuid4', function ($columnName) {
+            // WARNING! THIS WILL ONLY WORK IN POSTGRES WITH INSTALLED EXTENSION `uuid-ossp`
+            /**@var Blueprint $this */
+            return $this->uuid($columnName)->default(new Expression('uuid_generate_v4()'));
+        });
+    }
+}
